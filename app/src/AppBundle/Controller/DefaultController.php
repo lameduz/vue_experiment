69f6be39faa78fe764a37dbfdbc2ab8083ede074
La Meduze <mr.zenatimehdi@gmail.com>
2017-10-08 18:16:52 +0200

    [AppBundle] Added sample data array to work with

--- app/src/AppBundle/Controller/DefaultController.php
+++ app/src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,7 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $data = ['Sample 1', 'Sample 2', 'Sample 3', 'Sample 4'];
+        return $this->json($data);
     }
 }
